da61e9b21cf84e8798dde5665526bac3b5c2f7d4
Ilham Nopi Hendri <ilhamnopihendri@gmail.com>
2023-01-08 15:14:52 +0700

    add data contact

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
-const { pertanyaan, simpanContact } = require('./contacts');
+const { string } = require("yargs");
+const yargs = require("yargs");
+const { simpanContact } = require("./contacts");
 
-const main = async () => {
-    const nama = await pertanyaan('Masukan nama anda: ')
-    const email = await pertanyaan('Masukan email anda: ')
-    const noHp = await pertanyaan('Masukan nohp anda: ')
 
-    simpanContact(nama, email, noHp)
-}
 
-main()
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan kontak baru',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string'
+        },
+        noHp: {
+            describe: 'Nomor handphone',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler({ nama, email, noHp }) {
+        simpanContact(nama, email, noHp)
+    }
+
+})
+
+yargs.parse()
+
+// const { pertanyaan, simpanContact } = require('./contacts');
+
+// const main = async () => {
+//     const nama = await pertanyaan('Masukan nama anda: ')
+//     const email = await pertanyaan('Masukan email anda: ')
+//     const noHp = await pertanyaan('Masukan nohp anda: ')
+
+//     simpanContact(nama, email, noHp)
+// }
+
+// main()
--- contacts.js
+++ contacts.js
@@ -1,5 +1,7 @@
+const chalk = require('chalk');
 const fs = require('fs');
 const readline = require('readline');
+const validator = require('validator');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -14,23 +16,41 @@ if (!fs.existsSync(dirPath) || !fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
-const pertanyaan = (questions) => {
-    return new Promise((resolve, reject) => {
-        rl.question(questions, (data) => {
-            resolve(data)
-        })
-    })
-}
-
 const simpanContact = (nama, email, noHp) => {
     const contact = { nama, email, noHp };
     const file = fs.readFileSync(dataPath, 'utf-8')
     const contacts = JSON.parse(file);
+
+    // Cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama)
+    if (duplikat) {
+        console.log(chalk.red.inverse.bold("Contact sudah terdaftar gunakan nama lain"));
+        rl.close()
+        return false
+    }
+
+    // Cek email
+    const trueEmail = validator.isEmail(email);
+    if (email && !trueEmail) {
+        console.log(chalk.red.inverse.bold("email tidak valid !"));
+        rl.close()
+        return false
+    }
+
+    // Cek nohp
+    const trueNoHp = validator.isMobilePhone(noHp, 'id-ID')
+    if (!trueNoHp) {
+        console.log(chalk.red.inverse.bold("Nomor handphone tidak valid !"));
+        rl.close()
+        return false
+    }
+
     contacts.push(contact)
 
     fs.writeFileSync(dataPath, JSON.stringify(contacts, null, 2));
-    console.log('thanks to has input data');
-    rl.close();
+    console.log(chalk.green.inverse.bold('thanks to has input data'));
+    rl.close()
+
 }
 
-module.exports = { pertanyaan, simpanContact }
+module.exports = { simpanContact }
